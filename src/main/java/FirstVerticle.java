d6052321b2eaa2b31d7d0de2b7074fb8659336d4
kamleshsilag <kamleshsilag42@gmail.com>
2020-04-24 13:52:18 +0530

    Added Junits and Tested Application

---
+++ src/main/java/FirstVerticle.java
@@ -0,0 +1,22 @@
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class FirstVerticle extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> fut) {
+        vertx
+                .createHttpServer()
+                .requestHandler(r -> {
+                    r.response().end("<h1>Hello from my first " +
+                            "Vert.x 3 application</h1>");
+                })
+                .listen(9000, result -> {
+                    if (result.succeeded()) {
+                        fut.complete();
+                    } else {
+                        fut.fail(result.cause());
+                    }
+                });
+    }
+}
